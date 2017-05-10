a340ed0551cce504d874ee53493998685beda199
Hugo Maitre <hugo.maitre69@gmail.com>
2017-05-11 00:37:56 +0200

    Des tests

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var Sequelize = require('sequelize');
 var POSTGRESURI = require('./config/database.js').uri;
 var sequelize = new Sequelize(POSTGRESURI);
+
+var User = require("./models/user.js");
+
+
+var index = require('./routes/index')(express, User);
+var users = require('./routes/users');
 
 var app = express();
 
@@ -18,8 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- model/user.js
+++
@@ -1,7 +0,0 @@
-module.export = function(sequelize){
-	var User = sequelize.define('user', {
-		username: { type: Sequelize.STRING, allowNull: false },
-		birthday: Sequelize.DATE
-	});
-	return User;
-}
