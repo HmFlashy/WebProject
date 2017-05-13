0fbbd27319dce69ec21dd805cac0acaf128f135e
Hugo Maitre <hugo.maitre69@gmail.com>
2017-05-13 23:30:54 +0200

    Modif package.json +6

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static('../Client'));
+app.use(express.static('Client'));
 
 //app.all('/api/*', [require('./middleware/validatingAuthenticity.js')]);
 app.use('/', require('./routes')(pg));
