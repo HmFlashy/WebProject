0617a39e096ca649cd6549f2e4ef879fede7aea5
Hugo Maitre <hugo.maitre69@gmail.com>
2017-05-15 00:04:13 +0200

    Finalisation bd

--- database.sql
+++ database.sql
@@ -1,22 +1,71 @@
 ﻿CREATE TABLE Users (
-idUser serial,
-firstname varchar(30),
-lastname varchar(30),
-pseudo varchar(30),
-email varchar(100),
-password varchar(60),
-CONSTRAINT pk_users PRIMARY KEY (idUser)
+	idUser serial,
+	firstname varchar(32) NOT NULL,
+	lastname varchar(32) NOT NULL,
+	pseudo varchar(32) NOT NULL,
+	email varchar(64) NOT NULL,
+	password varchar(64) NOT NULL,
+	CONSTRAINT pk_users PRIMARY KEY (idUser),
+	CONSTRAINT un_users UNIQUE (pseudo, email)
 );
-DROP TABLE users;
 
-Truncate machine;
 CREATE TABLE Machine (
-idMachine serial,
-machineName varchar(50),
-description varchar(300),
-CONSTRAINT pk_machine PRIMARY KEY (idMachine)
+	idMachine serial,
+	nameMachine varchar(50) NOT NULL,
+	iduser integer NOT NULL,
+	CONSTRAINT pk_machine PRIMARY KEY (idMachine),
+	CONSTRAINT fk_machine_user FOREIGN KEY (iduser) REFERENCES users (iduser) ON DELETE CASCADE
 );
-DROP TABLE Machine;
-INSERT INTO machine VALUES (1, 'Banc dev-coucher', 'Banc permettant de faire des développés couchés');
-SELECT * FROM Machine WHERE idmachine=1;
-INSERT INTO Machine (machineName, description, userid) VALUES ('Lol','Ouai ok', 1);
+
+
+CREATE TABLE Exercice (
+	idExercice serial,
+	nameExercice varchar(32) NOT NULL,
+	descExercice varchar(128) NOT NULL,
+	idMachine integer,
+	iduser integer NOT NULL,
+	CONSTRAINT pk_exercice PRIMARY KEY (idExercice),
+	CONSTRAINT fk_exercice_user FOREIGN KEY (iduser) REFERENCES users (iduser) ON DELETE CASCADE,
+	CONSTRAINT fk_exercice_machine FOREIGN KEY (idmachine) REFERENCES machine (idmachine)
+);
+
+CREATE TABLE Training (
+	idTraining serial,
+	nameTraining varchar(32) NOT NULL,
+	descTraining varchar(128) NOT NULL,
+	idUser integer NOT NULL,
+	CONSTRAINT pk_training PRIMARY KEY (idTraining),
+	CONSTRAINT fk_training_user FOREIGN KEY (iduser) REFERENCES users (iduser) ON DELETE CASCADE
+);
+
+CREATE TABLE Performance (
+	idPerformance serial,
+	rating integer NOT NULL,
+	comment varchar(256) NOT NULL,
+	iduser integer NOT NULL,
+	CONSTRAINT pk_performance PRIMARY KEY (idPerformance),
+	CONSTRAINT fk_performance_user FOREIGN KEY (iduser) REFERENCES users (iduser) ON DELETE CASCADE
+);
+
+CREATE TABLE Contain (
+	idExercice integer NOT NULL,
+	idTraining integer NOT NULL,
+	numero integer NOT NULL,
+	last integer NOT NULL,
+	CONSTRAINT pk_contain PRIMARY KEY (idExercice, idTraining, numero),
+	CONSTRAINT fk_contain_training FOREIGN KEY (idTraining) REFERENCES Training (idTraining) ON DELETE CASCADE,
+	CONSTRAINT fk_contain_exercice FOREIGN KEY (idExercice) REFERENCES Exercice (idExercice) ON DELETE CASCADE
+);
+
+CREATE TABLE Series (
+	numberTimes integer NOT NULL,
+	numberEachTime integer NOT NULL
+)INHERITS(Contain);
+
+DROP TABLE users CASCADE;
+DROP TABLE Machine CASCADE;
+DROP TABLE Exercice CASCADE;
+DROP TABLE Training CASCADE;
+DROP TABLE Performance CASCADE;
+DROP TABLE Contain CASCADE;
+DROP TABLE Series CASCADE;
