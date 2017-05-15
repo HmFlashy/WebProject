cd436613c776ec7a998e71f2207ae648c6358889
HmFlashy <hugo.maitre69@gmail.com>
2017-05-15 15:21:28 +0200

    Saving

---
+++ routes/Performance.js
@@ -0,0 +1,25 @@
+module.exports = function(pg){
+   
+   var performance = {
+       getPerformances: function(req, res){
+           
+       },
+       
+       getPerformanceById: function(req, res){
+           
+       },
+       
+       //Get the last performaces, a number in the query tells the number of rows to get
+       getLastPerformances: function(req, res){
+           
+       },
+       
+       addPerformance: function(req, res){
+           
+       },
+       
+       deletePerformance: function(req, res){
+           
+       }
+   }
+}
