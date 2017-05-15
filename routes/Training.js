2474e2fad0923c9a801d6a4b0f4b11b56f8d3477
HmFlashy <hugo.maitre69@gmail.com>
2017-05-15 14:56:23 +0200

    Saving

---
+++ routes/Training.js
@@ -0,0 +1,26 @@
+module.exports = function(pg){
+    
+    var training = {
+        getTrainings: function(req, res){
+            return function(){
+                
+            }
+        },
+        
+        getTrainingById: function(req, res){
+            
+        },
+        
+        addTraining; function(req, res){
+            
+        },
+        
+        updateTraining: function(req, res){
+            
+        },
+        
+        deleteTraining: function(req, res){
+            
+        }
+    }
+}
